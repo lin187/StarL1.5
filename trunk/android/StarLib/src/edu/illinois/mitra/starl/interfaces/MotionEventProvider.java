198d4e992e0295a0d0a709f24dad97d24d27fc09
StarL <StarL@MITRA-WS>
2012-04-06 10:59:21 -0500

    Removed outdated StarLib components

--- trunk/android/StarLib/src/edu/illinois/mitra/starl/interfaces/MotionEventProvider.java
+++
@@ -1,6 +0,0 @@
-package edu.illinois.mitra.starl.interfaces;
-
-public interface MotionEventProvider {
-	public void addMotionListener(MotionListener l);
-	public void removeMotionListener(MotionListener l);
-}
--- trunk/android/StarLib/src/edu/illinois/mitra/starl/interfaces/MotionListener.java
+++
@@ -1,5 +0,0 @@
-package edu.illinois.mitra.starl.interfaces;
-
-public interface MotionListener {
-	public void motionEvent(int e);	
-}
