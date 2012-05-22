f1b76fc9a055d5cf02ae900ae40a3a8e5528a45f
ttj <devnull@localhost>
2012-05-22 10:46:02 -0500

    * fixing local merge problem with dropbox folder

--- trunk/android/StarLSim/src/edu/illinois/mitra/starlSim/main/Main.java
+++ trunk/android/StarLSim/src/edu/illinois/mitra/starlSim/main/Main.java
@@ -70,5 +70,12 @@ public class Main {
 			sim.enableDistacePredicate(SimSettings.PREDICATE_RADIUS, SimSettings.PREDICATE_OUT_DIR);
 			sim.start();
 		}
+				// TODO: close window on finish...
+			}
+		} else {
+			Simulation sim = new Simulation(SimSettings.N_BOTS, SimSettings.waypoint_file, SimSettings.initial_positions, SimSettings.app);
+			sim.enableDistacePredicate(SimSettings.PREDICATE_RADIUS, SimSettings.PREDICATE_OUT_DIR);
+			sim.start();
+		}
 	}	
 }
