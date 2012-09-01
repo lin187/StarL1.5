70bff477a596bd05b932e80eb2cbf956a2a3bb41
zimmrmn3 <devnull@localhost>
2012-09-01 17:47:31 -0500

    Added new project for movement example applications demonstrating the new simulator dataflow

---
+++ trunk/android/MovementExampleApps/src/edu/illinois/mitra/starlSim/simapps/Main.java
@@ -0,0 +1,23 @@
+package edu.illinois.mitra.starlSim.simapps;
+
+import edu.illinois.mitra.starlSim.main.SimSettings;
+import edu.illinois.mitra.starlSim.main.Simulation;
+
+public class Main {
+
+	private static boolean circles = false;
+
+	public static void main(String[] args) {
+		SimSettings settings = new SimSettings();
+		settings.N_BOTS = 1;
+
+		if(circles) {
+			Simulation sim = new Simulation(CircleMotion.class, settings);
+			sim.start();
+		} else {
+			Simulation sim = new Simulation(StraightLineMotion.class, settings);
+			sim.start();
+		}
+	}
+
+}
