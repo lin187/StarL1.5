76b76053d8e2c33b44fc7fddc3e97397b212491c
zimmrmn3 <devnull@localhost>
2012-09-02 11:21:41 -0500

    Updated template application to use external logicthread

--- trunk/android/StarlTemplate/src/edu/illinois/mitra/template/MainHandler.java
+++ trunk/android/StarlTemplate/src/edu/illinois/mitra/template/MainHandler.java
@@ -1,7 +1,5 @@
 package edu.illinois.mitra.template;
 
-import java.util.concurrent.Executors;
-
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
@@ -23,12 +21,10 @@ public class MainHandler extends Handler {
 	private CheckBox cbBluetooth;
 	private CheckBox cbRunning;
 	private TextView txtDebug;
-	
+
 	private GlobalVarHolder gvh;
-	
-	public MainHandler(RobotsActivity app, ProgressBar pbBluetooth,
-			ProgressBar pbBattery, CheckBox cbGPS, CheckBox cbBluetooth,
-			CheckBox cbRunning, TextView txtDebug) {
+
+	public MainHandler(RobotsActivity app, ProgressBar pbBluetooth, ProgressBar pbBattery, CheckBox cbGPS, CheckBox cbBluetooth, CheckBox cbRunning, TextView txtDebug) {
 		super();
 		this.app = app;
 		this.appContext = app.getApplicationContext();
@@ -39,46 +35,42 @@ public class MainHandler extends Handler {
 		this.cbRunning = cbRunning;
 		this.txtDebug = txtDebug;
 	}
-	
+
 	public void setGvh(GlobalVarHolder gvh) {
 		this.gvh = gvh;
 	}
-	
+
 	@Override
 	public void handleMessage(Message msg) {
 		super.handleMessage(msg);
-    	switch(msg.what) {
-    	case Common.MESSAGE_TOAST:
-    		Toast.makeText(appContext, msg.obj.toString(), Toast.LENGTH_LONG).show();
-    		break;
-    	case Common.MESSAGE_LOCATION:
-    		cbGPS.setChecked((Integer)msg.obj == Common.GPS_RECEIVING);
-    		break;
-    	case Common.MESSAGE_BLUETOOTH:
-    		pbBluetooth.setVisibility((Integer)msg.obj == Common.BLUETOOTH_CONNECTING?View.VISIBLE:View.INVISIBLE);
-    		cbBluetooth.setChecked((Integer)msg.obj ==  Common.BLUETOOTH_CONNECTED);
-    		break;
-    	case Common.MESSAGE_LAUNCH:
-    		app.launch(msg.arg1, msg.arg2);
-    		break;
-    	case Common.MESSAGE_ABORT:
-    		if(app.launched) {
-    			app.runThread.cancel();
-    	    	app.results.cancel(true);
-    			app.executor.shutdownNow();
-    			app.executor = Executors.newSingleThreadExecutor();
-    			app.runThread = new AppLogic(gvh);
-    		}
+		switch(msg.what) {
+		case Common.MESSAGE_TOAST:
+			Toast.makeText(appContext, msg.obj.toString(), Toast.LENGTH_LONG).show();
+			break;
+		case Common.MESSAGE_LOCATION:
+			cbGPS.setChecked((Integer) msg.obj == Common.GPS_RECEIVING);
+			break;
+		case Common.MESSAGE_BLUETOOTH:
+			pbBluetooth.setVisibility((Integer) msg.obj == Common.BLUETOOTH_CONNECTING ? View.VISIBLE : View.INVISIBLE);
+			cbBluetooth.setChecked((Integer) msg.obj == Common.BLUETOOTH_CONNECTED);
+			break;
+		case Common.MESSAGE_LAUNCH:
+			app.launch(msg.arg1, msg.arg2);
+			break;
+		case Common.MESSAGE_ABORT:
+			if(app.launched) {
+				app.abort();
+			}
 			gvh.plat.moat.motion_stop();
 			app.launched = false;
 			cbRunning.setChecked(false);
-    		break;
-    	case Common.MESSAGE_DEBUG:
-    		txtDebug.setText("DEBUG:\n" + (String) msg.obj);
-    		break;
-    	case Common.MESSAGE_BATTERY:
-    		pbBattery.setProgress((Integer) msg.obj);
-    		break;
-    	}	
+			break;
+		case Common.MESSAGE_DEBUG:
+			txtDebug.setText("DEBUG:\n" + (String) msg.obj);
+			break;
+		case Common.MESSAGE_BATTERY:
+			pbBattery.setProgress((Integer) msg.obj);
+			break;
+		}
 	}
 }
