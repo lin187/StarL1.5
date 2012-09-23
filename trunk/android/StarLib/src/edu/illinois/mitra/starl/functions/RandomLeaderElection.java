23fece61eb1d6b252851d7cb107a1f1e9af42705
zimmrmn3 <devnull@localhost>
2012-09-22 19:34:06 -0500

    Updated RandomLeaderElection algorithm to be threadless and generally less bad

--- trunk/android/StarLib/src/edu/illinois/mitra/starl/functions/RandomLeaderElection.java
+++ trunk/android/StarLib/src/edu/illinois/mitra/starl/functions/RandomLeaderElection.java
@@ -1,218 +1,147 @@
 package edu.illinois.mitra.starl.functions;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import edu.illinois.mitra.starl.comms.MessageContents;
 import edu.illinois.mitra.starl.comms.RobotMessage;
 import edu.illinois.mitra.starl.gvh.GlobalVarHolder;
 import edu.illinois.mitra.starl.interfaces.LeaderElection;
 import edu.illinois.mitra.starl.interfaces.MessageListener;
-import edu.illinois.mitra.starl.interfaces.StarLCallable;
 import edu.illinois.mitra.starl.objects.Common;
 
 /**
- * Elects a leader. All agents broadcast a random integer, the robot with the largest number wins the election.
- * Ties are broken by comparing agent identifiers lexicographically (word of the day). To ensure proper operation,
- * robots should be synchronized before electing!
+ * Leader election algorithm which picks a random robot from all of the
+ * participants. Calling 'elect' will begin the election process. Periodically
+ * calling 'getLeader' is required to advance the election state machine.
  * 
  * @author Adam Zimmerman
- * @version 1.0
- *
  */
-public class RandomLeaderElection extends StarLCallable implements LeaderElection, MessageListener {
+public class RandomLeaderElection implements LeaderElection, MessageListener {
 
-	private static final String TAG = "RandomLeaderElection";
-	private static final String ERR = "Critical Error";
-	private int nodes = 0;
-	private static int MAX_WAIT_TIME = 5000;
-	
-	private SortedSet<Ballot> ballots = new TreeSet<Ballot>();
-	private Set<String> receivedFrom = new HashSet<String>();
-	
-	private String announcedLeader;
-	
-	private ExecutorService executor = new ScheduledThreadPoolExecutor(1);
+	private static final int MAX_WAIT_TIME = 5000;
+	private static final String ERROR_RETURN = "ERROR";
+	private SortedSet<Ballot> ballots;
+	private Set<String> receivedFrom;
 
-	private Future<List<Object>> elected;
-	
+	private GlobalVarHolder gvh;
+
+	private static enum Stage {
+		WAIT_FOR_BALLOTS, WAIT_FOR_ANNOUNCEMENT, ERROR, DONE
+	};
+
+	private String leader;
+	private long startTime;
+	private int candidates;
+	private Stage stage;
+	private static Random rand = new Random();
+
 	public RandomLeaderElection(GlobalVarHolder gvh) {
-		super(gvh,"RandomLeaderElection");
-		registerListeners();
-
-		results = new String[1];
-	}
-
-	/**
-	 * Executor implementing random leader election
-	 */
-	@Override
-	public List<Object> callStarL() {
-		// clear sets between successive calls (needs to be here instead of above in class declaration)
-		announcedLeader = null;		
-		nodes = gvh.id.getParticipants().size();
-		gvh.trace.traceEvent(TAG, "Created", gvh.time());
-		
-		gvh.trace.traceEvent(TAG, "Beginning Election", gvh.time());
-		gvh.log.d(TAG, "Beginning election...");
-		nodes = gvh.id.getParticipants().size();
-		boolean error = false;
-
-		// Generate a random number
-		Random rand = new Random();
-		int myNum = rand.nextInt(1000);
-		receivedFrom.add(name);
-		ballots.add(new Ballot(name, myNum));
-
-		gvh.trace.traceVariable(TAG, "myNum", myNum, gvh.time());
-		gvh.log.i(TAG, "My number is " + myNum);
-		
-		// Broadcast
-		RobotMessage bcast = new RobotMessage("ALL", name, Common.MSG_RANDLEADERELECT, new MessageContents(myNum));
-		gvh.comms.addOutgoingMessage(bcast);
-		
-		// Wait to receive MSG_LEADERELECT messages
-		Long endTime = gvh.time()+MAX_WAIT_TIME;
-		gvh.trace.traceEvent(TAG, "Waiting for MSG_LEADERELECT messages", gvh.time());
-		while(!error && receivedFrom.size() < nodes) {
-			if(gvh.time() >= endTime) {
-				gvh.trace.traceEvent(TAG, "Waited timed out", gvh.time());
-				gvh.log.e(TAG, "Waited too long!");
-				
-				Set<String> ptc = new HashSet<String>(gvh.id.getParticipants());
-				ptc.removeAll(receivedFrom);
-				gvh.log.e(TAG, name + " has waited too long to receive election messages. Have only received " + receivedFrom.size() + ", missing ballots from " + ptc.toString());
-				error = true;
-			}
-			gvh.sleep(10);
-		}
-		
-		gvh.log.d(TAG, "Received all numbers, determining leader. Error = " + error);
-		// Determine the leader
-		String leader = null;
-		if(!error) {
-			gvh.log.d(TAG, "No errors, determining leader now.");
-			// Retrieve all names that submitted the largest random number, sort them
-			leader = ballots.first().toString();
-			gvh.trace.traceEvent(TAG, "Determined leader", leader, gvh.time());
-			
-			// Have determined a leader, broadcast the result
-			RobotMessage bcast_leader = new RobotMessage("ALL", name, Common.MSG_RANDLEADERELECT_ANNOUNCE, new MessageContents(leader));
-			gvh.comms.addOutgoingMessage(bcast_leader);
-			gvh.trace.traceEvent(TAG, "Notified all of leader", gvh.time());
-		}		
-		if(error) {
-			gvh.log.d(TAG, "An error occurred (waited too long?) must wait to receive announcement broadcasts.");
-			// Receive any MSG_LEADERELECT_ANNOUNCE messages, accept whoever they elect as leader
-			endTime = gvh.time()+MAX_WAIT_TIME;
-			gvh.trace.traceEvent(TAG, "Waiting for MSG_LEADERELECT_ANNOUNCE messages", gvh.time());
-			while(announcedLeader == null) {
-				if(gvh.time() > endTime) {
-					gvh.trace.traceEvent(TAG, "Waited timed out, leader election failed", gvh.time());
-					gvh.log.e(TAG, "Waited too long, leader election failed!");
-					results[0] = "ERROR";
-					return returnResults();
-				}
-				gvh.sleep(10);
-			}
-			leader = announcedLeader;
-		}
-		gvh.log.i(TAG, "Elected leader: " + leader);
-		gvh.trace.traceEvent(TAG, "Elected leader", leader, gvh.time());
-		results[0] = leader;
-		
-		receivedFrom.clear();
-		ballots.clear();
-		
-		return returnResults();
-	}
-	
-	public void messageReceied(RobotMessage m) {
-		gvh.log.i(TAG, "Received a message from " + m.getFrom() + ": " + m.getContents(0));
-		switch(m.getMID()) {
-		case Common.MSG_RANDLEADERELECT:
-			String from = m.getFrom();
-			if(receivedFrom.contains(from)) {
-				gvh.log.e(TAG, "Received from " + from + " twice!");
-			} else {
-				int val = Integer.parseInt(m.getContents(0));				
-				ballots.add(new Ballot(from, val));
-				receivedFrom.add(from);
-				gvh.log.i(TAG, "Received " + receivedFrom.size());
-				if(receivedFrom.size() == nodes) {
-					gvh.log.i(TAG, "READY TO ELECT A LEADER!");
-				}
-			}
-			gvh.trace.traceEvent(TAG, "Received MSG_RANDLEADERELECT message", m, gvh.time());
-			break;
-			
-		case Common.MSG_RANDLEADERELECT_ANNOUNCE:
-			announcedLeader = m.getContents(0);
-			gvh.trace.traceEvent(TAG, "Received MSG_RANDLEADERELECT_ANNOUNCE message", announcedLeader, gvh.time());
-			break;
-		}
-	}
-	
-	/**
-	 * Call the callStarL executor and initate leader election
-	 * 
-	 */
-	public void elect() {
-		elected = executor.submit(this);
-	}
-
-	/**
-	 * Return the name of the determined leader, if leader election is finished, or null if not
-	 * 
-	 */
-	@Override
-	public String getLeader() {
-		if(elected.isDone()) {
-			try {
-				return (String) elected.get().get(0);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
-			return "ERROR";
-		}
-		return null;
-	}
-	
-	private void registerListeners() {
+		this.gvh = gvh;
 		gvh.comms.addMsgListener(Common.MSG_RANDLEADERELECT, this);
 		gvh.comms.addMsgListener(Common.MSG_RANDLEADERELECT_ANNOUNCE, this);
 	}
 
-	private void unregisterListeners() {
-		gvh.comms.removeMsgListener(Common.MSG_RANDLEADERELECT);
-		gvh.comms.removeMsgListener(Common.MSG_RANDLEADERELECT_ANNOUNCE);		
+	@Override
+	public String getLeader() {
+		switch(stage) {
+		case WAIT_FOR_BALLOTS:
+			// If we've received all of the ballots, elect a leader and announce it
+			if(ballots.size() == candidates) {
+				leader = ballots.first().candidate;
+				stage = Stage.DONE;
+				announceLeader();
+				return leader;
+			}
+
+			// If we've timed out, wait for the announcement
+			if(gvh.time() - startTime > MAX_WAIT_TIME) {
+				stage = Stage.WAIT_FOR_ANNOUNCEMENT;
+				startTime = gvh.time();
+				break;
+			}
+			break;
+		case WAIT_FOR_ANNOUNCEMENT:
+			// If we've timed out again, go to the error state
+			if(gvh.time() - startTime > MAX_WAIT_TIME)
+				stage = Stage.ERROR;
+			break;
+		case ERROR:
+			return ERROR_RETURN;
+		case DONE:
+			return leader;
+		}
+		return null;
 	}
-	
+
+	@Override
+	public void elect() {
+		ballots = new TreeSet<Ballot>();
+		receivedFrom = new HashSet<String>();
+		stage = Stage.WAIT_FOR_BALLOTS;
+		candidates = gvh.id.getParticipants().size();
+		startTime = gvh.time();
+		leader = null;
+
+		int myNumber = rand.nextInt(1000);
+		RobotMessage myBallot = new RobotMessage("ALL", gvh.id.getName(), Common.MSG_RANDLEADERELECT, new MessageContents(myNumber));
+		gvh.comms.addOutgoingMessage(myBallot);
+		ballots.add(new Ballot(gvh.id.getName(), myNumber));
+	}
+
+	@Override
+	public void messageReceied(RobotMessage m) {
+		String from = m.getFrom();
+		switch(m.getMID()) {
+		case Common.MSG_RANDLEADERELECT:
+			if(stage != Stage.WAIT_FOR_BALLOTS)
+				return;
+
+			if(!receivedFrom.contains(from)) {
+				receivedFrom.add(from);
+				ballots.add(new Ballot(from, Integer.parseInt(m.getContents(0))));
+			}
+			break;
+		case Common.MSG_RANDLEADERELECT_ANNOUNCE:
+			if(stage != Stage.WAIT_FOR_ANNOUNCEMENT)
+				return;
+			// TODO: Make sure a quorum announced the same leader, otherwise error
+			leader = m.getContents(0);
+			stage = Stage.DONE;
+			break;
+		}
+	}
+
+	private void announceLeader() {
+		RobotMessage announce = new RobotMessage("ALL", gvh.id.getName(), Common.MSG_RANDLEADERELECT_ANNOUNCE, leader);
+		gvh.comms.addOutgoingMessage(announce);
+	}
+
+	@Override
+	public void cancel() {
+		gvh.comms.removeMsgListener(Common.MSG_RANDLEADERELECT);
+		gvh.comms.removeMsgListener(Common.MSG_RANDLEADERELECT_ANNOUNCE);
+	}
+
 	/**
-	 * Comparable class used to order agent votes lexicographically using agent identifiers 
+	 * Comparable class used to order agent votes lexicographically using agent
+	 * identifiers
 	 * 
 	 * @author Adam Zimmerman
-	 *
+	 * 
 	 */
 	private class Ballot implements Comparable<Ballot> {
 		public String candidate;
 		public int value;
-		
+
 		public Ballot(String candidate, int value) {
 			this.candidate = candidate;
 			this.value = value;
 		}
-		
+
 		public String toString() {
 			return candidate;
 		}
@@ -224,12 +153,5 @@ public class RandomLeaderElection extends StarLCallable implements LeaderElectio
 				return candidate.compareTo(other.candidate);
 			return value - other.value;
 		}
-	}
-	
-	@Override
-	public void cancel() {
-		executor.shutdownNow();
-		unregisterListeners();
-		gvh.trace.traceEvent(TAG, "Cancelled", gvh.time());
 	}
 }
