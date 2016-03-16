b2a25a424f0f359cfa8671e7fddc0b5db7385df6
ttj <taylor.johnson@gmail.com>
2016-03-16 16:15:43 -0500

    added csv logging in race app for demonstration purposes, can log position data of all robots for creating plots, need to refactor some and clean up, but for basic demonstration purposes for creating charts similar to those in the aiaa 2015 paper on flocking

--- trunk/android/RaceApp/src/edu/illinois/mitra/demo/race/Main.java
+++ trunk/android/RaceApp/src/edu/illinois/mitra/demo/race/Main.java
@@ -2,6 +2,10 @@ package edu.illinois.mitra.demo.race;
 
 import edu.illinois.mitra.starlSim.main.SimSettings;
 import edu.illinois.mitra.starlSim.main.Simulation;
+import edu.illinois.mitra.starlSim.data.CsvWriter;
+
+import java.io.*;
+import java.util.Set;
 
 public class Main {
 
@@ -15,10 +19,34 @@ public class Main {
 		settings.DRAW_WAYPOINT_NAMES(false);
 		settings.DRAWER(new RaceDrawer());
 
+        //settings.TIMEOUT(10);
+
+        settings.DRAW_TRACE(true);
+        settings.TRACE_OUT_DIR("test");
+        settings.USE_GLOBAL_LOGGER(true);
+
+
         settings.DRAW_ROBOT_STROKE_SIZE(25);
 		
 		Simulation sim = new Simulation(RaceApp.class, settings.build());
 		sim.start();
-	}
+
+
+        /*CsvWriter writer = null;
+        try {
+            writer = new CsvWriter("test5.csv", "N Robots",
+                    "Execution duration", "Requests made", "Assignments made",
+                    "Unpainted lines", "Total lines");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Object[] res = sim.getResults().get(0).toArray();
+        writer.commit(5, sim.getSimulationDuration() / 1000.0,
+                res[1], res[2], res[3]);
+
+        writer.close();*/
+
+    }
 
 }
