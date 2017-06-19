3a29f7ccfda44580accdffea2fce65c5e78bbf8d
Yinghui Yang <yinghui.yang@vanderbilt.edu>
2017-06-19 10:03:46 -0500

    clean the build

--- ClientLib/build/generated/source/aidl/debug/com/o3dr/services/android/lib/model/ICommandListener.java
+++ ClientLib/build/generated/source/aidl/debug/com/o3dr/services/android/lib/model/ICommandListener.java
@@ -1,10 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
-<<<<<<< HEAD
  * Original file: D:\\6-9Starl\\starl\\ClientLib\\src\\main\\java\\com\\o3dr\\services\\android\\lib\\model\\ICommandListener.aidl
-=======
- * Original file: C:\\Users\\wangcs2\\Desktop\\EhangFinal\\starl\\ClientLib\\src\\main\\java\\com\\o3dr\\services\\android\\lib\\model\\ICommandListener.aidl
->>>>>>> b0b84a086f2b80e9b37a77f85273819698a3c74c
  */
 package com.o3dr.services.android.lib.model;
 /**
---
+++ ClientLib/build/generated/source/buildConfig/release/com/o3dr/android/client/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.o3dr.android.client;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.o3dr.android.client";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 300200;
+  public static final String VERSION_NAME = "3.0.2";
+  // Fields from default config.
+  public static final int LOG_FILE_LEVEL = 3;
+  public static final boolean SITL_DEBUG = false;
+  public static final String SOLO_LINK_IP = "10.1.1.10";
+  public static final boolean WRITE_LOG_FILE = false;
+}
