a46592728b3e054fe8a3d11ec7511018c2934dd1
Yinghui Yang <yinghui.yang@vanderbilt.edu>
2017-07-13 15:26:47 -0500

    change 3DR for SimSettings

--- starLSim/src/main/java/edu/illinois/mitra/starlSim/main/SimSettings.java
+++ starLSim/src/main/java/edu/illinois/mitra/starlSim/main/SimSettings.java
@@ -291,7 +291,7 @@ public class SimSettings {
 		private String QUADCOPTER_NAME = "quadcopter";
 		private String GHOST_NAME = "ghostie";
         private String MAVIC_NAME = "mavic";
-		private String o3DR_NAME = "3DRSolo";
+		private String o3DR_NAME = "Solo";
 		private int BOT_RADIUS = 165;
 		private String TRACE_OUT_DIR;
 		private boolean THREE_D = true;
